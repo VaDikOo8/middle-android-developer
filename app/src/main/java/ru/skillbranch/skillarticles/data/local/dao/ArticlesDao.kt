79bcd76c0627bc8cb74f9d75a1f13704de7b4650
VaDikOo8 <vadikoo8.patron@gmail.com>
2020-08-31 01:11:17 +0300

    fix after tests

--- app/src/main/java/ru/skillbranch/skillarticles/data/local/dao/ArticlesDao.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/local/dao/ArticlesDao.kt
@@ -24,7 +24,7 @@ interface ArticlesDao : BaseDao<Article> {
         SELECT * FROM articles
     """
     )
-    fun findArticles(): List<Article>
+    fun findArticles(): LiveData<List<Article>>
 
     @Query(
         """
@@ -32,14 +32,14 @@ interface ArticlesDao : BaseDao<Article> {
         WHERE id = :id
     """
     )
-    fun findArticleById(id: String): Article
+    fun findArticleById(id: String): LiveData<Article>
 
     @Query(
         """
         SELECT * FROM ArticleItem
     """
     )
-    fun findArticleItems(): List<ArticleItem>
+    fun findArticleItems(): LiveData<List<ArticleItem>>
 
     @Delete
     fun delete(article: Article)
@@ -50,7 +50,7 @@ interface ArticlesDao : BaseDao<Article> {
         WHERE category_id IN(:categoryIds)
     """
     )
-    fun findArticleItemsByCategoryIds(categoryIds: List<String>): List<ArticleItem>
+    fun findArticleItemsByCategoryIds(categoryIds: List<String>): LiveData<List<ArticleItem>>
 
     @Query(
         """
@@ -59,7 +59,7 @@ interface ArticlesDao : BaseDao<Article> {
         WHERE refs.t_id = :tag
     """
     )
-    fun findArticlesByTagId(tag: String): List<ArticleItem>
+    fun findArticlesByTagId(tag: String): LiveData<List<ArticleItem>>
 
     @RawQuery(observedEntities = [ArticleItem::class])
     fun findArticlesByRaw(simpleSQLiteQuery: SimpleSQLiteQuery): DataSource.Factory<Int, ArticleItem>
