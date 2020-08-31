aa4923af105fcce9db2527104bf8d682ff7ecec2
VaDikOo8 <vadikoo8.patron@gmail.com>
2020-08-31 09:39:28 +0300

    fix after tests

--- app/src/main/java/ru/skillbranch/skillarticles/data/local/dao/ArticlePersonalInfosDao.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/local/dao/ArticlePersonalInfosDao.kt
@@ -30,7 +30,7 @@ interface ArticlePersonalInfosDao : BaseDao<ArticlePersonalInfo> {
             WHERE article_id = :articleId
         """
     )
-    fun findArticlePersonalInfo(articleId: String): LiveData<ArticlePersonalInfo>
+    fun findPersonalInfos(articleId: String): LiveData<ArticlePersonalInfo>
 
     @Query(
         """
